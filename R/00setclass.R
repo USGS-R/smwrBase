fb120ff2577967c692cec84f0f0fb0f31f0e1747
Dave Lorenz <lorenz@usgs.gov>
2014-08-06 11:31:35 -0500

    more changes for roxygen 4.0

--- R/00setclass.R
+++
@@ -1,9 +0,0 @@
-#' timeDay class
-#'
-#' The timeDay class is a simple representation of the time of day. The
-#'data are stores as seconds since midnight.
-#'
-#' @name timeDay-class
-#' @rdname timeDay-class
-#' @exportClass timeDay
-setClass("timeDay", representation(time="numeric", format="character"))
